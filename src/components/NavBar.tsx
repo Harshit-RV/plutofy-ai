d968c1f37c0a2bf4d565d2db027a2d3b2fbf4561
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-02-04 15:36:23 +0530

    NavBar Plutofy logo navigates to app.plutofy.live

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,19 @@
 import { SignedIn, UserButton } from "@clerk/clerk-react";
 import { Button } from "antd";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 // import { FeedbackComponent } from "./FeedbackComponent";
 
 export const NavBar = () => {
     const location = useLocation();
+    const navigate = useNavigate();
   
     return (
         <SignedIn>  
           <div className="w-full h-14 bg-white items-end flex px-4 sm:px-8 md:px-16 lg:px-20 justify-between">
                 
                 <div className="flex h-full">
-                <img src="/Plutofy-Logo.png" onClick={()=> window.open("https://plutofy.live/", "_blank")}  alt="image" className="rounded-t-sm bg-[#553566] min-w-12 flex items-end mt-4 mr-6 px-2.5 aspect-square object-contain hover:cursor-pointer" />
+                <img src="/Plutofy-Logo.png" onClick={()=> navigate('/')}  alt="image" className="rounded-t-sm bg-[#553566] min-w-12 flex items-end mt-4 mr-6 px-2.5 aspect-square object-contain hover:cursor-pointer" />
+                {/* <img src="/Plutofy-Logo.png" onClick={()=> window.open("https://plutofy.live/", "_blank")}  alt="image" className="rounded-t-sm bg-[#553566] min-w-12 flex items-end mt-4 mr-6 px-2.5 aspect-square object-contain hover:cursor-pointer" /> */}
                   <div className="sm:flex h-full hidden">
                     <NavBarItem pathname={location.pathname} link="/" title="Home" />
                     <NavBarItem pathname={location.pathname} link="/create" title="Create Agent" />
