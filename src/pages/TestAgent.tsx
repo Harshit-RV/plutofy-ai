9be20f64b590423de34ed342becf6e66cdec86c7
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-02-12 21:07:55 +0530

    Added Tabs with sections for Usage and Settings, currently defaults to Coming Soon.. on TestAgent Screen

--- src/pages/TestAgent.tsx
+++ src/pages/TestAgent.tsx
@@ -14,6 +14,7 @@ import { useQuery } from "react-query";
 import { JsonValue } from "@/components/JsonInput";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { useAuth } from "@clerk/clerk-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const AgentCreate = ({ isTestMode = false } : { isTestMode: boolean }) => {
   const { getToken } = useAuth();
@@ -126,7 +127,7 @@ const AgentCreate = ({ isTestMode = false } : { isTestMode: boolean }) => {
               <h2 className="w-full bg-white mx-1 my-1 sm:my-2 font-bold text-lg sm:text-xl border-none">
                 {agent?.name} 
               </h2>
-              <p className="w-full bg-white mx-1 mt-2  text-gray-500 focus-visible:outline-gray-300 text-sm border-none">
+              <p className="w-full bg-white mx-1 mt-1  text-gray-500 focus-visible:outline-gray-300 text-sm border-none">
                 {agent?.description} 
               </p>
             </div>
@@ -134,84 +135,104 @@ const AgentCreate = ({ isTestMode = false } : { isTestMode: boolean }) => {
             <ButtonCN onClick={() => navigate(`/edit/${agentDocId}`)} variant={'outline'} className="w-[130px] sm:flex hidden border-gray-300">Edit</ButtonCN>
           </div>
 
-          <div className="flex flex-col gap-7 flex-grow px-3 sm:px-10 md:px-14 xl:px-48 pt-6 w-full">
-
-              <Collapsible
-                open={isConfigOpen}
-                onOpenChange={setIsConfigOpen}
-                className="w-full rounded-xl border space-y-2 bg-white p-4"
-              >
-                <div className="flex items-center space-x-4 px-1">
-                  <h4 className="text-sm font-semibold">
-                    Agent Config
-                  </h4>
-                  <CollapsibleTrigger asChild>
-                    <ButtonCN variant="ghost" size="sm">
-                      <ChevronsUpDown className="h-4 w-4" />
-                      <span className="sr-only">Toggle</span>
-                    </ButtonCN>
-                  </CollapsibleTrigger>
-                </div>
-                <CollapsibleContent className="flex flex-col lg:flex-row gap-4 px-0 lg:px-0">
-                  <div className="lg:w-2/3 flex-grow p-2 lg:p-4 border rounded-lg text-sm text-gray-600">
-                    {agent?.instruction} 
-                  </div>
-                  <pre className="bg-gray-100 border p-4 text-sm rounded-md overflow-auto">{JSON.stringify(getJsonObject(agent?.outputStructure ?? []), null, 2)}</pre>
-                </CollapsibleContent>
-              </Collapsible>
-
-            <div className="flex mb-10">
-              <div className="w-full flex flex-col lg:flex-row h-min gap-5 ">
-                
-                <div className="flex flex-col lg:w-1/2 gap-1">
-                  <h3 className="text-md text-gray-500 font-semibold">Input</h3>
-                  <pre>
-                    <Textarea 
-                      value={input}
-                      onChange={(e) => setInput(e.target.value)}
-                      className="drop-shadow-none shadow-sm min-h-[230px]"
-                    />
-                  </pre>
-                  <div className="flex justify-end gap-3 mt-2">
-                    <ButtonCN variant={'outline'} size={'sm'} onClick={onClear}>Clear</ButtonCN>
-
-                    <ButtonCN className="" disabled={loading} size={'sm'} onClick={getResponse}>
-                      {loading ? (
-                        <Loader2 className="animate-spin w-20"/> 
-                      ) : (
-                        <p> Get Response</p>
-                      )
-                      }
-                    </ButtonCN>
-                  </div>
-                </div>
-
-                <div className="flex flex-col lg:w-1/2 gap-2">
-                  <div className="flex items-center gap-5">
-                    <h3 className="text-md text-gray-500 font-semibold">Output</h3>
-                    {(responseInfo) && (
-                      <div className="flex px-4 py-1 bg-white gap-5  border rounded-lg text-sm text-gray-600">
-                        <p>
-                          Status: <span 
-                            className={responseInfo.statusCode == 200 || responseInfo.statusCode == 201 ? 'text-green-600 font-bold' : 'text-red-600 font-bold'}>
-                              {responseInfo.statusCode}
-                          </span>
-                        </p>
-                        <p>Time: <span 
-                            className={'text-green-600 font-bold'}>
-                              {responseInfo.responseTime} ms
-                          </span> </p>
-                      </div>
-                    )}
-                  </div>
-                  <div className="flex flex-col-reverse gap-2">
-                    <pre className="bg-white border p-4 min-h-[230px] h-full text-sm rounded-md overflow-auto">{output}</pre>
-                    
-                  </div>
-                </div>
-
+          <div className="flex flex-col gap-5 flex-grow px-3 sm:px-10 md:px-14 xl:px-48 pt-4 w-full">
+            <Collapsible
+              open={isConfigOpen}
+              onOpenChange={setIsConfigOpen}
+              className="w-full rounded-xl shadow-sm border space-y-2 bg-white py-2.5 px-4"
+            >
+              <div className="flex items-center space-x-4 px-1">
+                <h4 className="text-sm font-semibold">
+                  Agent Config
+                </h4>
+                <CollapsibleTrigger asChild>
+                  <ButtonCN variant="ghost" size="sm">
+                    <ChevronsUpDown className="h-4 w-4" />
+                    <span className="sr-only">Toggle</span>
+                  </ButtonCN>
+                </CollapsibleTrigger>
               </div>
-            </div>
+              <CollapsibleContent className="flex flex-col lg:flex-row gap-4 px-0 lg:px-0">
+                <div className="lg:w-2/3 flex-grow p-2 lg:p-4 border rounded-lg text-sm text-gray-600">
+                  {agent?.instruction} 
+                </div>
+                <pre className="bg-gray-100 border p-4 text-sm rounded-md overflow-auto">{JSON.stringify(getJsonObject(agent?.outputStructure ?? []), null, 2)}</pre>
+              </CollapsibleContent>
+            </Collapsible>
+
+            <Tabs defaultValue={isTestMode ? "test" : "usage"}>
+              <TabsList className="bg-gray-200 border-gray-300 border">
+                <TabsTrigger className="px-3" value="usage">Usage</TabsTrigger>
+                <TabsTrigger className="px-4" value="test">Test</TabsTrigger>
+                <TabsTrigger className="px-3" value="settings">Settings</TabsTrigger>
+              </TabsList>
+
+              <TabsContent value="usage" className="pt-3">
+                <div className="bg-white w-full flex justify-center py-10 items-center rounded-lg border">
+                  Coming Soon..
+                </div>
+              </TabsContent>
+              <TabsContent value="settings" className="pt-3">
+                <div className="bg-white w-full flex justify-center py-10 items-center rounded-lg border">
+                  Coming Soon..
+                </div>
+              </TabsContent>
+              <TabsContent value="test" className="flex flex-col gap-7 flex-grow pt-4 w-full">
+                <div className="flex mb-10">
+                  <div className="w-full flex flex-col lg:flex-row h-min gap-5 ">
+                    
+                    <div className="flex flex-col lg:w-1/2 gap-1">
+                      <h3 className="text-md text-gray-500 font-semibold">Input</h3>
+                      <pre>
+                        <Textarea 
+                          value={input}
+                          onChange={(e) => setInput(e.target.value)}
+                          className="drop-shadow-none shadow-sm min-h-[230px]"
+                        />
+                      </pre>
+                      <div className="flex justify-end gap-3 mt-2">
+                        <ButtonCN variant={'outline'} size={'sm'} onClick={onClear}>Clear</ButtonCN>
+
+                        <ButtonCN className="" disabled={loading} size={'sm'} onClick={getResponse}>
+                          {loading ? (
+                            <Loader2 className="animate-spin w-20"/> 
+                          ) : (
+                            <p> Get Response</p>
+                          )
+                          }
+                        </ButtonCN>
+                      </div>
+                    </div>
+
+                    <div className="flex flex-col lg:w-1/2 gap-2">
+                      <div className="flex items-center gap-5">
+                        <h3 className="text-md text-gray-500 font-semibold">Output</h3>
+                        {(responseInfo) && (
+                          <div className="flex px-4 py-1 bg-white gap-5  border rounded-lg shadow-sm text-sm text-gray-600">
+                            <p>
+                              Status: <span 
+                                className={responseInfo.statusCode == 200 || responseInfo.statusCode == 201 ? 'text-green-600 font-bold' : 'text-red-600 font-bold'}>
+                                  {responseInfo.statusCode}
+                              </span>
+                            </p>
+                            <p>Time: <span 
+                                className={'text-green-600 font-bold'}>
+                                  {responseInfo.responseTime} ms
+                              </span> </p>
+                          </div>
+                        )}
+                      </div>
+                      <div className="flex flex-col-reverse gap-2 shadow-sm">
+                        <pre className="bg-white border p-4 min-h-[230px] h-full text-sm rounded-md overflow-auto">{output}</pre>
+                        
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </TabsContent>
+              
+            </Tabs>
           </div>
       </div>
     ) : (
