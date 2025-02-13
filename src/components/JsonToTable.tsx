02003dcec748afbe90afff3c7f3c86756e63caf7
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-02-14 03:06:51 +0530

    Non-developer Testing mode with table view instead of JSON

---
+++ src/components/JsonToTable.tsx
@@ -0,0 +1,54 @@
+import type React from "react"
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table"
+import { Card, CardContent } from "@/components/ui/card"
+
+type JsonValue = string | number | boolean | null | JsonObject | JsonArray
+type JsonObject = { [key: string]: JsonValue }
+type JsonArray = JsonValue[]
+
+interface JsonTableProps {
+  data: JSON
+}
+
+const JsonTable: React.FC<JsonTableProps> = ({ data }) => {
+  const renderValue = (value: JsonValue): React.ReactNode => {
+    if (typeof value === "object" && value !== null) {
+      return <JsonTable data={JSON.parse(JSON.stringify(value))} />
+    }
+    return String(value)
+  }
+
+  const renderTableRows = (obj: JsonObject | JsonArray) => {
+    if (Array.isArray(obj)) {
+      return obj.map((item, index) => (
+        <TableRow key={index}>
+          <TableCell className="font-bold">{index}</TableCell>
+          <TableCell>{renderValue(item)}</TableCell>
+        </TableRow>
+      ))
+    }
+
+    return Object.entries(obj).map(([key, value]) => (
+      <TableRow key={key}>
+        <TableCell className="font-semibold text-gray-500 bg-gray-50 px-3">{key}</TableCell>
+        <TableCell className="py-3 pl-3">{renderValue(value)}</TableCell>
+      </TableRow>
+    ))
+  }
+
+  if (typeof data !== "object" || data === null) {
+    return <span>{String(data)}</span>
+  }
+
+  return (
+    <Card className="w-full shadow-sm">
+      <CardContent className="pt-4">
+        <Table>
+          <TableBody>{renderTableRows(JSON.parse(JSON.stringify(data)))}</TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default JsonTable
--- src/pages/TestAgent.tsx
+++ src/pages/TestAgent.tsx
@@ -15,12 +15,23 @@ import { JsonValue } from "@/components/JsonInput";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { useAuth } from "@clerk/clerk-react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import JsonTable from "@/components/JsonToTable";
+import { Switch } from "@/components/ui/switch"
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+enum TestingMode {
+  DEVELOPER_MODE = "developer_mode",
+  NORMAL_MODE = "normal_mode",
+}
 
 const AgentCreate = ({ isTestMode = false } : { isTestMode: boolean }) => {
   const { getToken } = useAuth();
   const { agentDocId } = useParams();
   const [ input, setInput ] = useState<string>("");
   const [ output, setOutput ] = useState<string>("");
+  const [ testingMode, setTestingMode ] = useState<TestingMode>(TestingMode.DEVELOPER_MODE);
+
   const [ loading, setLoading ] = useState<boolean>(false);
   const [ isConfigOpen, setIsConfigOpen ] = useState<boolean>(isTestMode ? false : true);
   const [ responseInfo, setResponseInfo ] = useState<{responseTime: number, statusCode: number} | null>(null);
@@ -96,6 +107,19 @@ const AgentCreate = ({ isTestMode = false } : { isTestMode: boolean }) => {
     setLoading(false);
   }
 
+  const updateInputWithMessage = (message: string) => {
+    try {
+      const json = JSON.parse(input);
+      json.message = message; 
+      setInput(JSON.stringify({...json}))
+    } catch (error) {
+      console.error("Invalid JSON input:", error);
+      return input;
+    }
+  }
+
+  const pagePadding = "px-3 sm:px-10 md:px-20 xl:px-40";
+
   if (agentError) {
     return (
       <div className='flex flex-col justify-center pb-[150px] items-center font-mono min-h-screen bg-gray-100 px-2.5 sm:px-6 md:px-10 lg:px-0'>
@@ -122,7 +146,7 @@ const AgentCreate = ({ isTestMode = false } : { isTestMode: boolean }) => {
   return (
     !agentLoading || agent != undefined ? (
       <div className='flex flex-col font-mono min-h-screen bg-gray-100'>
-          <div className=" flex justify-between items-center bg-white border px-3 sm:px-10 md:px-20 xl:px-48 pt-5 w-full py-5">
+          <div className={`flex justify-between items-center bg-white border ${pagePadding} pt-5 w-full py-5`}>
             <div className="flex flex-col w-full pr-20">
               <h2 className="w-full bg-white mx-1 my-1 sm:my-2 font-bold text-lg sm:text-xl border-none">
                 {agent?.name} 
@@ -135,7 +159,7 @@ const AgentCreate = ({ isTestMode = false } : { isTestMode: boolean }) => {
             <ButtonCN onClick={() => navigate(`/edit/${agentDocId}`)} variant={'outline'} className="w-[130px] sm:flex hidden border-gray-300">Edit</ButtonCN>
           </div>
 
-          <div className="flex flex-col gap-5 flex-grow px-3 sm:px-10 md:px-14 xl:px-48 pt-4 w-full">
+          <div className={`flex flex-col gap-5 flex-grow ${pagePadding} pt-4 w-full`}>
             <Collapsible
               open={isConfigOpen}
               onOpenChange={setIsConfigOpen}
@@ -160,6 +184,7 @@ const AgentCreate = ({ isTestMode = false } : { isTestMode: boolean }) => {
               </CollapsibleContent>
             </Collapsible>
 
+           
             <Tabs defaultValue={isTestMode ? "test" : "usage"}>
               <TabsList className="bg-gray-200 border-gray-300 border">
                 <TabsTrigger className="px-3" value="usage">Usage</TabsTrigger>
@@ -168,26 +193,63 @@ const AgentCreate = ({ isTestMode = false } : { isTestMode: boolean }) => {
               </TabsList>
 
               <TabsContent value="usage" className="pt-3">
-                <div className="bg-white w-full flex justify-center py-10 items-center rounded-lg border">
+                <div className="bg-white text-sm w-full flex justify-center py-10 items-center rounded-lg border">
                   Coming Soon..
                 </div>
               </TabsContent>
               <TabsContent value="settings" className="pt-3">
-                <div className="bg-white w-full flex justify-center py-10 items-center rounded-lg border">
+                <div className="bg-white text-sm w-full flex justify-center py-10 items-center rounded-lg border">
                   Coming Soon..
                 </div>
               </TabsContent>
               <TabsContent value="test" className="flex flex-col gap-7 flex-grow pt-4 w-full">
-                <div className="flex mb-10">
-                  <div className="w-full flex flex-col lg:flex-row h-min gap-5 ">
-                    
+                <div className="flex items-center space-x-2">
+                  <Switch id="airplane-mode" checked={testingMode == TestingMode.DEVELOPER_MODE} onCheckedChange={(val) => setTestingMode(val ? TestingMode.DEVELOPER_MODE : TestingMode.NORMAL_MODE)}/>
+                  <Label htmlFor="airplane-mode">Developer Mode</Label>
+                </div>
+                
+                {testingMode == TestingMode.NORMAL_MODE ? (
+                  <div className="flex flex-col gap-0 mb-10">
+                    <div className="flex flex-col gap-1">
+                      <h3 className="text-md text-gray-500 font-semibold">Input</h3>
+                      <pre>
+                        <Input 
+                          value={JSON.parse(input)["message"]}
+                          onChange={(e) => updateInputWithMessage(e.target.value)}
+                          placeholder="Enter message here"
+                          className="drop-shadow-sm shadow-md rounded-lg py-7 px-4"
+                        />
+                      </pre>
+                      <div className="flex justify-end gap-3 mt-2">
+                        <ButtonCN variant={'outline'} size={'sm'} onClick={onClear}>Clear</ButtonCN>
+
+                        <ButtonCN className="" disabled={loading} size={'sm'} onClick={getResponse}>
+                          {loading ? (
+                            <Loader2 className="animate-spin w-20"/> 
+                          ) : (
+                            <p> Get Response</p>
+                          )
+                          }
+                        </ButtonCN>
+                      </div>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                      <h3 className="text-md text-gray-500 font-semibold">Output</h3>
+                      <JsonTable 
+                        data={output ? JSON.parse(output) : {}}
+                      />
+                    </div>
+                  </div>
+                ) : (
+                  <div className="w-full mb-10 flex flex-col lg:flex-row h-min gap-5 ">
                     <div className="flex flex-col lg:w-1/2 gap-1">
                       <h3 className="text-md text-gray-500 font-semibold">Input</h3>
                       <pre>
                         <Textarea 
                           value={input}
                           onChange={(e) => setInput(e.target.value)}
-                          className="drop-shadow-none shadow-sm min-h-[230px]"
+                          className="drop-shadow-none shadow-sm border-2 min-h-[230px]"
                         />
                       </pre>
                       <div className="flex justify-end gap-3 mt-2">
@@ -229,7 +291,7 @@ const AgentCreate = ({ isTestMode = false } : { isTestMode: boolean }) => {
                     </div>
 
                   </div>
-                </div>
+                )}
               </TabsContent>
               
             </Tabs>
