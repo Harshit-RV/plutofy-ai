4e25ef9ff69c8474b6f52978afd9a3db83335175
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-09-11 19:10:47 +0530

    fix ui (minor): make AgentCard height uniform

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -141,7 +141,7 @@ const AgentCard = ({
 }) => {
   return (
     <Link to={`/agent/${agentDocId}`}>
-    <Card>
+    <Card className="pb-5 h-full flex flex-col justify-between">
       <CardHeader className="pt-6 pb-3">
         <CardTitle className="flex justify-between">
           <h2 className="font-black text-md">{name}</h2>
@@ -167,7 +167,7 @@ const AgentCard = ({
       <CardContent className="py-0 pb-4">
         <p className="text-sm text-gray-500">{truncateString(description)} </p>
       </CardContent>
-      <CardFooter className="flex gap-4 pb-4 w-full justify-between">
+      <CardFooter className="flex gap-4 py-0 w-full justify-between">
         <p className="bg-gray-100 border border-gray-200 px-3 xl:px-4 rounded-full flex items-center justify-center text-xs h-7">{model}</p>
         <div className="flex gap-2 bg-gray-50 border border-gray-200 rounded-full items-center h-7 px-3 xl:px-4">
           <div className="w-2 h-2 bg-green-600 rounded-full"></div>
