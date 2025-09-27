bac6f6798faca69ba1233d0be705001411e2b2d3
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-09-27 12:33:27 +0530

    api: add optional displayName field to OutputStructure to make it consistent with InputField from workflow-scheme
    - this is done so we can reuse dynamic zod schema generation functions for workflows

--- api/src/models/Agent.model.ts
+++ api/src/models/Agent.model.ts
@@ -6,6 +6,7 @@ export type PrimitiveType = "string" | "number" | "boolean" | "object" | "array"
 export interface OutputStructure {
   id: string;
   name: string;
+  displayName?: string;
   type: PrimitiveType;
   fields?: OutputStructure[];
 }
